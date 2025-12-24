d7c5cd8f9ff57d8f683bff65d2c4865e76fcc24e
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2025-12-24 07:02:31 +0530

    Adding Toastify to cart and show cart

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,9 @@ import { ShopContext } from "../context/ShopContext";
 
 const NavBar = () => {
   const [visible, setVisible] = useState(false);
+
+  const { getToCart } = useContext(ShopContext);
+
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <img to={"/"} src={assets.logo} className="w-36" alt="" />
@@ -47,7 +50,7 @@ const NavBar = () => {
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
-            10
+            {getToCart()}
           </p>
         </Link>
         <img
--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,16 +1,59 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
   const currency = "$";
   const delivery_fee = 10;
+  const [cartItems, setCartItems] = useState({});
 
+  const addToCart = async (itemId,size) => {
+
+    if(!size){
+      toast.error("Please select a size");
+      return;
+    }
+
+    let cartData = structuredClone(cartItems);
+
+    if(cartData[itemId]){
+      if(cartData[itemId][size]){
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else{
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    setCartItems(cartData);
+  };
+
+  const getToCart = () => {
+    let totalCount = 0;
+    for(const items in cartItems){
+      for(const item in cartItems[items]){
+        try{
+          if(cartItems[items][item] > 0){
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {
+          console.error("Error calculating cart total:", error);
+        }
+      }
+    }
+    return totalCount;
+  }
+ 
   const value = {
     products,
     currency,
     delivery_fee,
+    cartItems,
+    addToCart,
+    getToCart,
   };
 
   return (
